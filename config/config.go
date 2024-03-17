284b0888bf37159fa7f6647527b2c3b81f6ec471
mgodunov <mikhailgodunow@gmail.com>
2024-03-17 11:18:03 +0300

    Config change

--- config/config.go
+++ config/config.go
@@ -5,8 +5,10 @@ import (
 )
 
 type Config struct {
+	AppPort int `mapstructure:"APP_PORT"`
+
 	DBHost string `mapstructure:"DB_HOST"`
-	DBPort string `mapstructure:"DB_PORT"`
+	DBPort int `mapstructure:"DB_PORT"`
 	DBUser string `mapstructure:"DB_USER"`
 	DBPass string `mapstructure:"DB_PASS"`
 	DBName string `mapstructure:"DB_NAME"`
